dc361d4fd43cc12e9410e326d2a8ede3f4e23db9
Sabin <spradh@users.noreply.github.com>
2024-01-30 16:17:34 -0600

    Create 3 Augmented Dicky-Fuller.py

---
+++ 1 ARIMA Models/3 Augmented Dicky-Fuller.py
@@ -0,0 +1,14 @@
+# Import augmented dicky-fuller test function
+from statsmodels.tsa.stattools import adfuller
+
+# Run test
+result = adfuller(earthquake['earthquakes_per_year'])
+
+# Print test statistic
+print(result[0])
+
+# Print p-value
+print(result[1])
+
+# Print critical values
+print(result[4]) 
