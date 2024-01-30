38823d207b8b4cb1d740561cb44884cf8520dc1c
Sabin <spradh@users.noreply.github.com>
2024-01-30 16:06:01 -0600

    Update 2 Train-test splits.py

--- 1 ARIMA Models/2 Train-test splits.py
+++ 1 ARIMA Models/2 Train-test splits.py
@@ -1,5 +1,5 @@
 # Split the data into a train and test set
-candy_train = candy.loc[: '2007-01-01']
+candy_train = candy.loc[: '2006-12-31'] #exercise is incorrect - this should be 2001-01-01/ there is a break in the data
 candy_test = candy.loc['2007-01-01':]
 
 # Create an axis
