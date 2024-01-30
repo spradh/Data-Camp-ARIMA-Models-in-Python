54e178e38e68fe57644d7b457a87a6f0fb46c4ad
Sabin <spradh@users.noreply.github.com>
2024-01-30 16:08:42 -0600

    Update 2 Train-test splits.py

--- 1 ARIMA Models/2 Train-test splits.py
+++ 1 ARIMA Models/2 Train-test splits.py
@@ -1,5 +1,5 @@
 # Split the data into a train and test set
-candy_train = candy.loc[: '2006-12-31'] #exercise is incorrect - this should be 2001-01-01/ there is a break in the data
+candy_train = candy.loc[: '2006-12-31'] #exercise is incorrect - this should be 2001-01-01/ there is a break in the data -> it doesn't matter for this exercise since the data is monthly captured on the 1st of eery month but wouldn't work for daily level data.
 candy_test = candy.loc['2007-01-01':]
 
 # Create an axis
