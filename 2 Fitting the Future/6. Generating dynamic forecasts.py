43ecd21626ffb6f26339ae4f6cf8052867a9c319
Sabin <spradh@users.noreply.github.com>
2024-01-30 21:06:25 -0600

    Create 6. Generating dynamic forecasts.py

---
+++ 2 Fitting the Future/6. Generating dynamic forecasts.py
@@ -0,0 +1,15 @@
+# Generate predictions
+dynamic_forecast = results.get_prediction(steps=-30, dynamic=True)
+
+# Extract prediction mean
+mean_forecast = dynamic_forecast.predicted_mean
+
+# Get confidence intervals of predictions
+confidence_intervals = dynamic_forecast.conf_int()
+
+# Select lower and upper confidence limits
+lower_limits = confidence_intervals.loc[:,'lower close']
+upper_limits = confidence_intervals.loc[:,'upper close']
+
+# Print best estimate predictions
+print(mean_forecast)
