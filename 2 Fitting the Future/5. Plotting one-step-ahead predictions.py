c9b417e778e2263b892319d6e882cadf993da98f
Sabin <spradh@users.noreply.github.com>
2024-01-30 21:04:24 -0600

    Create 5. Plotting one-step-ahead predictions.py

---
+++ 2 Fitting the Future/5. Plotting one-step-ahead predictions.py
@@ -0,0 +1,15 @@
+# plot the amazon data
+plt.plot(amazon.index, amazon, label='observed')
+
+# plot your mean predictions
+plt.plot(mean_forecast.index, mean_forecast, color='r', label='forecast')
+
+# shade the area between your confidence limits
+plt.fill_between(mean_forecast.index, lower_limits,
+		 upper_limits, color='pink')
+
+# set labels, legends and show plot
+plt.xlabel('Date')
+plt.ylabel('Amazon Stock Price - Close USD')
+plt.legend()
+plt.show()
