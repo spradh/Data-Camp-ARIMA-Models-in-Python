ef5ce6798f8521519d77b9aa32ea48f59aebfea5
Sabin <spradh@users.noreply.github.com>
2024-01-31 08:58:09 -0600

    Create 1. AR or MA.py

---
+++ 3 The Best of the Best Models/1. AR or MA.py
@@ -0,0 +1,13 @@
+# Import
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# Create figure
+fig, (ax1, ax2) = plt.subplots(2,1, figsize=(12,8))
+ 
+# Plot the ACF of df
+plot_acf(df, lags=10, zero=False, ax=ax1)
+
+# Plot the PACF of df
+plot_pacf(df, lags=10, zero=False, ax=ax2)
+
+plt.show()
